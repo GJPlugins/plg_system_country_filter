0a1e6940b47fc327166d976f490b5a1ac41b654f
oleg <sad.net79@gmail.com>
2020-05-23 15:33:41 +0300

    v0.0.4

--- sql/country_filter.install.sql
+++ sql/country_filter.install.sql
@@ -1,4 +1,16 @@
 -- ---------------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `#__plg_system_country_filter_ip`
+(
+    `id`         int(11)          NOT NULL AUTO_INCREMENT,
+    `id_map`     int(11)          NOT NULL,
+    `ip`         int(11) UNSIGNED NOT NULL COMMENT 'INET_ATON AND INET_NTOA',
+    `created`    datetime         NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `last_visit` datetime         NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `ip` (`ip`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='Справочник IP адресов';
+
 --
 -- Справочник названий Стран
 --
--- sql/country_filter.uninstall.sql
+++ sql/country_filter.uninstall.sql
@@ -8,6 +8,13 @@ DROP TABLE IF EXISTS `#__plg_system_country_filter_regions`;
 -- Справочник названий городов
 DROP TABLE IF EXISTS `#__plg_system_country_filter_cities`;
 --
+-- Справочник IP Адресов
+DROP TABLE IF EXISTS `#__plg_system_country_filter_ip`;
+
+
+--
 -- Сводная таблица для города
 DROP TABLE IF EXISTS `#__plg_system_country_filter_city_map`;
+
+
 
